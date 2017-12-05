40a07a135a980c750ff11e89b25689db261b2910
Paige Boyer <paigeboyer@Paiges-MacBook-Pro.local>
2017-12-05 11:44:36 -0500

    stlyed comments section in show.ejs

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,4 @@
 
-// edit image preview
-// adapted from https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-// const previewImgs = () => {
-//   let preview = document.querySelector('#preview');
-//   let hiddenImg = document.querySelector('#hidden-img');
-//   let files = document.querySelector('input[type=file]').files;
-//
-//   const readAndPreview = (file) => {
-//     // Make sure `file.name` matches our extensions criteria
-//     if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
-//       let reader = new FileReader();
-//
-//       reader.addEventListener("load", function () {
-//         let arr = [];
-//         let image = new Image();
-//         image.title = file.name;
-//         image.src = this.result;
-//         preview.appendChild(image);
-//         arr.push(image.src);
-//         console.log(arr);
-//         console.log(hiddenImg);
-//         hiddenImg.value = image.src;
-//       }, false);
-//
-//       reader.readAsDataURL(file);
-//     }
-//   }
-//
-//   if (files) {
-//     [].forEach.call(files, readAndPreview);
-//   }
-// }
-
-
-
 $(() => {
 
   // image hover behavior
@@ -50,8 +15,5 @@ $(() => {
 
   // assigning class for card images
   $('.edit-row').children('.index-card').eq(1).addClass('middle');
-
-
-  // logging preview image source
 
 })
